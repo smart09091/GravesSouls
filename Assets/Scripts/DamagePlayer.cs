91d1a85293fa2cd98386e4873583e84badcaa739
Adrian Alcomendras <cloud102000@gmail.com>
2020-10-26 12:08:34 +0800

    Edit: Added fire asset and getting hit animation

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GravesSouls{
+    public class DamagePlayer : MonoBehaviour
+    {
+        public int damage = 25;
+
+        private void OnTriggerEnter(Collider other){
+            PlayerStats playerStats = other.GetComponent<PlayerStats>();
+
+            if(playerStats != null){
+                playerStats.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GravesSouls{
+    public class PlayerStats : MonoBehaviour
+    {
+        public int healthLevel = 10;
+        public int maxHealth;
+        public int currentHealth;
+
+        public HealthBar healthBar;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            maxHealth = SetMaxHealthFromHealthLevel();
+            currentHealth = maxHealth;
+            healthBar.SetMaxHealth(maxHealth);
+        }
+
+        private int SetMaxHealthFromHealthLevel(){
+            maxHealth = healthLevel * 10;
+            return maxHealth;
+        }
+
+        public void TakeDamage(int damage){
+            currentHealth = currentHealth - damage;
+
+            healthBar.SetCurrentHealth(currentHealth);
+        }
+    }
+}
